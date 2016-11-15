924c0aea0506db9a7a2b3402664751763cf853d1
Job Chong <jobchong@gmail.com>
2016-11-15 11:39:16 +0800

    home route should be first one listed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "users#home"
+  
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -18,6 +20,5 @@ Rails.application.routes.draw do
     resources :search
   end
 
-  root "users#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
