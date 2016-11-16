00301ae617ef8343e931a3eb50cbd4dc3a91eed8
Henson Tay <hensontay@gmail.com>
2016-11-16 16:19:21 +0800

    modified search controller and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
 def home
-@allTasks = Task.all
+  @tasks = Task.all
+  @creatorTasks = Task.with_role(:creator, current_user)
+  @appliedTasks = Task.with_role(:applicant, current_user)
+  @workingTasks = Task.with_role(:worker)
+  @otherTasks = @tasks - @creatorTasks
+  @otherTasks = @otherTasks - @appliedTasks
+  @allTasks = @otherTasks - @workingTasks
 end
 
 def show
