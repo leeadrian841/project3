01628b9e97e3e59a263a0fd2d320a1fd80ec1814
Adrian Lee <leeadrian841@gmail.com>
2016-11-17 20:52:55 +0800

    clean code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
 def home
   @tasks = Task.all
-  @creatorTasks = Task.with_role(:creator, current_user)
+  # @creatorTasks = Task.with_role(:creator, current_user)
   @appliedTasks = Task.with_role(:applicant, current_user)
   @workingTasks = Task.with_role(:worker)
   @completedTasks = Task.where("completed_worker= ? AND completed_creator= ?", true, true)
-  @otherTasks = @tasks - @creatorTasks
-  @otherTasks = @otherTasks - @appliedTasks
+  # @otherTasks = @tasks - @creatorTasks
+  @otherTasks = @tasks - @appliedTasks
   @otherTasks = @otherTasks - @workingTasks
   @allTasks = @otherTasks - @completedTasks
 end
